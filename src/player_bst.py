e89aa6cb712bf3ba5435ef7f038efe1972e498a7
“YAleksandr <Y20129072@tafe.wa.edu.au>
2025-06-03 11:00:40 +0800

    feat(search): Add search method to PlayerBST class

--- src/player_bst.py
+++ src/player_bst.py
@@ -16,7 +16,7 @@ class PlayerBST:
             self._insert_recursively(self._root, player)
 
     def _insert_recursively(self, current_node, player):
-        if player.score < current_node.player.score:
+        if player.name < current_node.player.name:
             if current_node.left is None:
                 current_node.left = PlayerBNode(player)
             else:
@@ -26,3 +26,29 @@ class PlayerBST:
                 current_node.right = PlayerBNode(player)
             else:
                 return self._insert_recursively(current_node.right, player)
+
+    def search(self, name):
+        if self._root is None:
+            return self._root
+        elif self._root.player.name == name:
+            return self._root.player
+        else:
+            result = self._search_recursively(self._root, name)
+            return result
+
+    def _search_recursively(self, current_node, name):
+        if name.lower() < current_node.player.name.lower():
+            if current_node.left is None:
+                return None
+            elif current_node.left.player.name.lower() == name.lower():
+                return current_node.left.player
+            else:
+                return self._search_recursively(current_node.left, name)
+        else:
+            if current_node.right is None:
+                return None
+            elif current_node.right.player.name.lower() == name.lower():
+                return current_node.right.player
+            else:
+                return self._search_recursively(current_node.right, name)
+
--- tests/test_player_bst.py
+++ tests/test_player_bst.py
@@ -9,7 +9,7 @@ class TestPlayer(unittest.TestCase):
         self.player_bst = PlayerBST()
 
     def test_insert_players(self):
-        players = [Player('01', "Alice", score=10), Player('02', "Bob",  score=7),
+        players = [Player('01', "Bob", score=10), Player('02', "Alice",  score=7),
                    Player('03', "Charlie", score=15)]
 
         for player in players:
@@ -21,8 +21,8 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(self.root.left.player.uid, '02')
         self.assertEqual(self.root.right.player.uid, '03')
 
-    def test_insert_players_same_scores(self):
-        players = [Player('01', "Alice", score=10), Player('02', "Bob",  score=10),
+    def test_insert_players_same_names(self):
+        players = [Player('01', "Alice", score=10), Player('02', "Alice",  score=10),
                    Player('03', "Charlie", score=7)]
 
         for player in players:
@@ -32,7 +32,32 @@ class TestPlayer(unittest.TestCase):
 
         self.assertEqual(self.root.player.uid, '01')
         self.assertEqual(self.root.right.player.uid, '02')
-        self.assertEqual(self.root.left.player.uid, '03')
+        self.assertEqual(self.root.right.right.player.uid, '03')
+
+
+    def test_search_players(self):
+        players = [Player('01', "Bob", score=10), Player('02', "Alice",  score=7),
+                   Player('03', "Charlie", score=15)]
+
+        for player in players:
+            self.player_bst.insert(player)
+
+        self.player_object = self.player_bst.search("Alice")
+
+        self.assertEqual(self.player_object.name, 'Alice')
+
+    def test_search_players_non_existing_name(self):
+        players = [Player('01', "Alice", score=10), Player('02', "Bob",  score=10),
+                   Player('03', "Charlie", score=7)]
+
+        for player in players:
+            self.player_bst.insert(player)
+
+        self.root = self.player_bst.root
+
+        self.player_object = self.player_bst.search("Alex")
+
+        self.assertIsNone(self.player_object)
 
 
 
